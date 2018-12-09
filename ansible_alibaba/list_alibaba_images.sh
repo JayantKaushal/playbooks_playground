7f892bc995af64370f24aa03ec104e2655dd8715
jayantkaushal <awaken.rogue@gmail.com>
2018-12-09 17:13:28 +0530

    removing some extra content

--- ansible_alibaba/list_alibaba_images.sh
+++
@@ -1,2 +0,0 @@
-ansible-playbook  /home/cmpdev/ansible_playbooks/playbooks/ansible_alibaba/list_images.yaml   -vvv -e '{"http_port":8080,"https_port":443,"admin_username":"jayant","admin_password":"jayant"}'
-
--- ansible_alibaba/list_alibaba_instance_types.sh
+++
@@ -1,2 +0,0 @@
-ansible-playbook  /home/cmpdev/ansible_playbooks/playbooks/ansible_alibaba/site.yaml   -vvv -e '{"http_port":8080,"https_port":443,"admin_username":"jayant","admin_password":"jayant"}'
-
