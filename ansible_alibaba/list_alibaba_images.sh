4e2172181c4cf173355d25437cfdfe3713003072
cmp <cmpdev@itomcmp.servicenow.com>
2018-12-09 05:58:35 -0500

    some more playbooks

---
+++ ansible_alibaba/list_alibaba_images.sh
@@ -0,0 +1,2 @@
+ansible-playbook  /home/cmpdev/ansible_playbooks/playbooks/ansible_alibaba/list_images.yaml   -vvv -e '{"http_port":8080,"https_port":443,"admin_username":"jayant","admin_password":"jayant"}'
+
---
+++ ansible_alibaba/list_alibaba_instance_types.sh
@@ -0,0 +1,2 @@
+ansible-playbook  /home/cmpdev/ansible_playbooks/playbooks/ansible_alibaba/site.yaml   -vvv -e '{"http_port":8080,"https_port":443,"admin_username":"jayant","admin_password":"jayant"}'
+
